366d46fa29c7130c806c2c99c8ef9de1fd8b7aff
iculmination <ovsiichuk.bohdan@gmail.com>
2025-01-14 20:00:03 +0200

    shadcn setup + auth-form template + auth route

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="center bg-amber-100 ">
-      <h1 className="h1">
-        Cloud - The only storage solution you need.
-      </h1>
+      <h1 className="h1">Cloud - The only storage solution you need.</h1>
     </div>
   );
 }
